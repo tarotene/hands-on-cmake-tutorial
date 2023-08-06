cda223b4468df844f5f26b78033c3445f0eff8d8
tarotene <tarotene.dev@gmail.com>
2023-08-07 01:30:35 +0900

    Format C++

--- Step2/MathFunctions/MathFunctions.cxx
+++ Step2/MathFunctions/MathFunctions.cxx
@@ -6,10 +6,9 @@
 #include "mysqrt.h"
 
 namespace mathfunctions {
-double sqrt(double x)
-{
+double sqrt(double x) {
   // TODO 9: If USE_MYMATH is defined, use detail::mysqrt.
   // Otherwise, use std::sqrt.
   return detail::mysqrt(x);
 }
-}
+} // namespace mathfunctions
